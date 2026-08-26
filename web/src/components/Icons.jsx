96629d269dc682f55b656bb34a79d3281f5c930e
Rodrigo Onorati <onoratirodrigo@yahoo.com.ar>
2026-08-26 19:57:06 -0300

    Revertir icono de actualizar en boton de nueva actualizacion

--- web/src/components/Icons.jsx
+++ web/src/components/Icons.jsx
@@ -53,14 +53,3 @@ export function IconoReloj() {
     </svg>
   );
 }
-
-export function IconoActualizar() {
-  return (
-    <svg {...props} className="h-4 w-4">
-      <path d="M3 12a9 9 0 0 1 15-6.7L21 8" />
-      <path d="M21 3v5h-5" />
-      <path d="M21 12a9 9 0 0 1-15 6.7L3 16" />
-      <path d="M3 21v-5h5" />
-    </svg>
-  );
-}
